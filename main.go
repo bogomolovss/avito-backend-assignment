179c8ffef444b8712ad094ccaf4a3717371dfd7a
bogomolovss <bogomolovqs@gmail.com>
2023-08-29 16:39:24 +0300

    add database connect implementation

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+	"user-segments/database"
+	"user-segments/model"
+
+	"github.com/joho/godotenv"
+)
+
+func loadDatabase() {
+	database.Connect()
+	database.Database.AutoMigrate(&model.User{})
+	database.Database.AutoMigrate(&model.Segment{})
+}
+
+func loadEnv() {
+	err := godotenv.Load(".env.local")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
+func main() {
+	loadEnv()
+	loadDatabase()
+}
